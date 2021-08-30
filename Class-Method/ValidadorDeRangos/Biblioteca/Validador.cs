3aaba092435a27b4083f0a4cb97066afea771ea4
EmanuelCamejo <emakme@gmail.com>
2021-08-30 04:09:19 -0300

    Validador de rangos

---
+++ Class-Method/ValidadorDeRangos/Biblioteca/Validador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Validador
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+             bool retorno;
+            
+                if (valor <= max && valor >= min)
+                {
+                    retorno = true;
+                }
+                else 
+                {
+                    retorno= false;
+                }
+            return retorno;
+            //return  if (valor <= max && valor >= min);
+        }
+
+    }
+}
---
+++ Class-Method/ValidadorDeRangos/Ejercicio1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            bool esNumero;
+            bool rango;
+            int acumNumero = 0;
+            int contNumeros = 0;
+            int valorMax = int.MinValue;
+            int ValorMin = int.MaxValue;
+            double prom;
+
+            while (contNumeros < 10)
+            {
+                Console.WriteLine("Ingrese un numero");
+                esNumero = int.TryParse(Console.ReadLine(), out numero);
+
+                if (esNumero)
+                {
+
+                    rango = Validador.Validar(numero, -100, 100);
+
+                    if (rango)
+                    {
+                        if (numero > valorMax)
+                        {
+                            valorMax = numero;
+                        }
+                        if (numero < ValorMin)
+                        {
+                            ValorMin = numero;
+                        }
+                        acumNumero += numero;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No esta en el rango");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Es no es un número");
+                }
+                contNumeros++;
+            }
+            prom = acumNumero / 10;
+
+            Console.WriteLine("El maximo es {0}, el minimo es {1}, el promedio es {2}", valorMax, ValorMin, prom);
+        }
+    }
+}
