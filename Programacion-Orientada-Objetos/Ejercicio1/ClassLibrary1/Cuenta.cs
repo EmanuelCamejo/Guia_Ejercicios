30ce2fb2d6f4edfeb69a4657af9d8b825850bbe1
EmanuelCamejo <emakme@gmail.com>
2021-08-30 20:32:46 -0300

    CreoQueNecesitoUnPrestamo

---
+++ Programacion-Orientada-Objetos/Ejercicio1/ClassLibrary1/Cuenta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Cuenta
+    {
+        private string titular;
+        private decimal cantidad;
+        private string mostrar;
+        private int ingresar;
+        private int retirar;
+
+        public string GetMostrar()
+        {
+            return mostrar;
+        }
+
+        public int GetIngresar()
+        {
+            return ingresar;
+        }
+        public int GetRetirar()
+        {
+            return retirar;
+        }
+
+
+        public Cuenta(string mostrar, int ingresar, int retirar) 
+        {
+            this.mostrar = mostrar;
+            this.ingresar = ingresar;
+            this.retirar = retirar;
+        }
+
+        
+
+    }
+}
---
+++ Programacion-Orientada-Objetos/Ejercicio1/CreoQueNecesitoUnPrestamo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Biblioteca;
+
+namespace CreoQueNecesitoUnPrestamo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta Uno = new Cuenta("Pepe S.A.", 200, 100);
+
+            
+            Console.WriteLine($"{Uno.GetMostrar()}");
+        }
+    }
+}
