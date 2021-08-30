4fa125b10ff135098070903e2d64c0cec6022ddc
EmanuelCamejo <emakme@gmail.com>
2021-08-30 04:35:14 -0300

    ¿Desea continuar?

---
+++ Class-Method/DeseaContinuar/Biblioteca/Validador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Validador
+    {
+        public static bool ValidarRespuesta(string respuesta)
+        {
+            bool res;
+
+            if (respuesta == "S" || respuesta == "s")
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Class-Method/DeseaContinuar/Ejercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int acum = 0;
+            string respuesta;
+
+            do
+            {
+                Console.WriteLine("Ingresar un número entero para ser sumado");
+
+                if (int.TryParse(Console.ReadLine(), out num))
+                {
+                    acum += num;
+                }
+                else
+                {
+                    Console.WriteLine("Error, Reingrese un número\n");
+                }
+
+                Console.WriteLine("¿Desea continuar? (S/N)");
+                respuesta = Console.ReadLine();
+                Console.WriteLine();
+
+
+            } while (Validador.ValidarRespuesta(respuesta));
+            
+
+            Console.WriteLine("La suma de los números ingresados es: {0}",acum);
+
+
+        }
+    }
+}
