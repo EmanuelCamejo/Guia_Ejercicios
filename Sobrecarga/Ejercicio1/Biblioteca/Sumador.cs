162fff8f4ae5be790bff82a66b15199623bdc95a
EmanuelCamejo <emakme@gmail.com>
2021-08-31 20:46:48 -0300

    Sumador

---
+++ Sobrecarga/Ejercicio1/Biblioteca/Sumador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+        private long num1, num2, resultado;
+
+        #region Contructores
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador() : this(0)
+        {
+
+        }
+
+        #endregion
+
+        #region Metodos
+        public long Sumar(long num1, long num2)
+        {
+            cantidadSumas++;
+            long resultado = num1 + num2;
+            return resultado;
+        }
+        public string Sumar(string a, string b)
+        {
+            return a + b;
+        }
+        #endregion
+
+        public static explicit operator Sumador(long cantidaSumas)
+         {
+
+            return cantidaSumas;
+         }
+
+}
+}
