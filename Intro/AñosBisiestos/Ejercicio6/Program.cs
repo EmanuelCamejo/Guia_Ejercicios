b1b0ace136a9fef17a5aa3c81d52aa31ede455d9
EmanuelCamejo <emakme@gmail.com>
2021-09-04 08:42:08 -0300

    Años bisiestos

---
+++ Intro/AñosBisiestos/Ejercicio6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bisiesto=0;
+            Console.WriteLine("Ingresar rango de años para saber los bisiestos");
+            
+            Console.WriteLine("Ingresar año Inicio");
+            int anoIin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar año Fin");
+            int anoFin = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = anoIin; i <= anoFin; i++)
+            {
+                if (i % 4 == 0)
+                {
+
+                    if (i % 100 == 0)
+                    {
+                        if (i % 400 == 0)
+                        {
+                            bisiesto = i;
+                            Console.WriteLine(@"{0} es bisiesto", bisiesto);
+                        }
+                        else
+                        {
+                            //Console.WriteLine("{0} no es bisiesto", i);
+                        }
+                    }
+                    else
+                    {
+                        bisiesto = i;
+                        Console.WriteLine(@"{0} es bisiesto", bisiesto);
+                    }
+                }
+                else 
+                {
+                    //Console.WriteLine("{0} no es bisiesto", i);
+                }
+                
+            }
+
+        }
+    }
+}
