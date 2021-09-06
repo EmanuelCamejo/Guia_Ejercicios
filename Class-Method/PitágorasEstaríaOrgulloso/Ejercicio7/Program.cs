5c61f305b0b53337235238dc47c07c1b162f2e55
EmanuelCamejo <emakme@gmail.com>
2021-09-06 02:38:28 -0300

    Pitágoras estaría orgulloso

---
+++ Class-Method/PitágorasEstaríaOrgulloso/Ejercicio7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     Pitágoras estaría orgulloso\n");
+
+            Console.WriteLine("Ingrese la base del triangulo a calcular");
+            double basse = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura del triangulo a calcular");
+            double altura = double.Parse(Console.ReadLine());
+
+            double resultado = Math.Sqrt(Math.Pow(2, basse) + Math.Pow(2, altura));
+
+            Console.WriteLine($"El resultado es: {resultado}");
+        }
+    }
+}
