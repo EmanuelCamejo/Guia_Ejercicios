87259427c612822b1a6d5fd58c95e51ddf25e3d2
EmanuelCamejo <emakme@gmail.com>
2021-09-06 04:01:48 -0300

    Creo que necesito un prestamo

---
+++ Programacion-Orientada-Objetos/CreoQueNecesitoUnPréstamo/Biblioteca/Cuenta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Cuenta
+    {
+        private string titular;
+        private double cantidad;
+
+        #region Metodos
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular()
+        {
+            return titular;
+        }
+        public double GetCantidad()
+        {
+            return cantidad;
+        }
+
+        
+
+        public string Mostrar()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Razon Social: {titular}\nMonto actual: {cantidad} ");
+            return stringBuilder.ToString();
+        }
+
+        public void Ingresar(double num)
+        {
+            if (num > 0)
+            {
+                this.cantidad+=num;
+            }
+        }
+
+        public void Retirar(double num)
+        {
+            if (num > 0)
+            {
+                this.cantidad -= num;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Programacion-Orientada-Objetos/CreoQueNecesitoUnPréstamo/Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la Razon Social");
+            string razonSocial=Console.ReadLine();
+            Console.WriteLine("Que monto desea agregar a su cuenta");
+            double monto = double.Parse(Console.ReadLine());
+            
+            Cuenta c1 = new Cuenta(razonSocial, monto);
+            Console.WriteLine(c1.Mostrar());
+
+            c1.Ingresar(100);
+            Console.WriteLine(c1.Mostrar());
+            c1.Retirar(50);
+            Console.WriteLine(c1.Mostrar());
+
+        }
+    }
+}
