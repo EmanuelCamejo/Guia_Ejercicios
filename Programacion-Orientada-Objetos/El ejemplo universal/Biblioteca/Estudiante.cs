f5b9e6c3cd3803409259c3529c8c0429d273d60c
EmanuelCamejo <emakme@gmail.com>
2021-09-06 04:41:30 -0300

    Un ejemplo universal

---
+++ Programacion-Orientada-Objetos/El ejemplo universal/Biblioteca/Estudiante.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        #region Metodos
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void SetNotaPrimerParcial(int notaPrimerParcial)
+        {
+            this.notaPrimerParcial = notaPrimerParcial;
+        }
+        public void SetNotaSegundoParcial(int notaSegundoParcial)
+        {
+            this.notaSegundoParcial = notaSegundoParcial;
+        }
+
+        private float CalcularPromedio()
+        {
+            return (this.notaPrimerParcial + this.notaSegundoParcial) / 2;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            if (this.notaPrimerParcial>=4 && this.notaSegundoParcial>=4)
+            {
+                return random.Next(6,11);
+            }
+            return -1;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Alumno {this.nombre}, {this.apellido}, con legajo:{this.legajo}");
+            stringBuilder.AppendLine($"Nota primer parcial: {this.notaPrimerParcial} Nota segundo parcial: {this.notaSegundoParcial}");
+            stringBuilder.AppendLine($"El promedio es: {CalcularPromedio()}");
+            if (CalcularNotaFinal() != -1)
+            {
+                stringBuilder.AppendLine($"Aprobado, nota final: {CalcularNotaFinal()}");
+                
+            }
+            else
+            {
+                stringBuilder.AppendLine($"Desaprobado");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Programacion-Orientada-Objetos/El ejemplo universal/Ejercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante e1 = new Estudiante("Emanuel","Camejo","00001");
+            Estudiante e2 = new Estudiante("Ulises", "Camejo", "00002");
+            Estudiante e3 = new Estudiante("Yamila", "Valenzisi", "00003");
+            e1.SetNotaPrimerParcial(3);
+            e1.SetNotaSegundoParcial(6);
+
+            e2.SetNotaPrimerParcial(7);
+            e2.SetNotaSegundoParcial(7);
+
+            e3.SetNotaPrimerParcial(10);
+            e3.SetNotaSegundoParcial(9);
+
+
+            Console.WriteLine(e1.Mostrar());
+            Console.WriteLine(e2.Mostrar());
+            Console.WriteLine(e3.Mostrar());
+
+
+        }
+    }
+}
